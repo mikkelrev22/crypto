4c2e6b60d784bcebe0af02bc4a1b8c6ea41dac99
Michael Thompson <michael.220488@gmail.com>
2019-08-14 10:49:13 -0700

    working on api call

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      
+    }
+    
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
--- server.js
+++ server.js
@@ -6,4 +6,19 @@ const bodyParser = require('body-parser')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../client/dist'))
+app.get('https://api.coindesk.com/v1/bpi/historical/close.json', (req, res)=>{
+
+
+  if (error) return next(error)
+  res.status(200).send()
+})
+app.use((req, res, next) => {
+  res.send(404)
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.stack)
+})
 app.listen(Port, ()=>{console.log(`Port ${Port} is listening, yo`)})
